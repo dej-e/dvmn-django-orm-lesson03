28abd6db50cdd8bd9c98267444544676d85f08ea
Mike Katsupko <t63723@gmail.com>
2019-09-20 11:41:46 +0700

    init commit

---
+++ script.py
@@ -0,0 +1,85 @@
+from datacenter.models import (
+    Schoolkid,
+    Mark,
+    Lesson,
+    Chastisement,
+    Commendation
+)
+import random
+
+
+COMMENDATIONS = [
+    'Молодец!',
+    'Отлично!',
+    'Хорошо!',
+    'Гораздо лучше, чем я ожидал!',
+    'Ты меня приятно удивил!',
+    'Великолепно!',
+    'Прекрасно!',
+    'Ты меня очень обрадовал!',
+    'Именно этого я давно ждал от тебя!',
+    'Сказано здорово – просто и ясно!',
+    'Ты, как всегда, точен!',
+    'Очень хороший ответ!',
+    'Талантливо!',
+    'Ты сегодня прыгнул выше головы!',
+    'Я поражен!',
+    'Уже существенно лучше!',
+    'Потрясающе!',
+    'Замечательно!',
+    'Прекрасное начало!',
+    'Так держать!',
+    'Ты на верном пути!',
+    'Здорово!',
+    'Это как раз то, что нужно!',
+    'Я тобой горжусь!',
+    'С каждым разом у тебя получается всё лучше!',
+    'Мы с тобой не зря поработали!',
+    'Я вижу, как ты стараешься!',
+    'Ты растешь над собой!',
+    'Ты многое сделал, я это вижу!',
+    'Теперь у тебя точно все получится!',
+]
+
+
+def get_child(child_name):
+    return Schoolkid.objects.get(full_name__contains=child_name)
+
+
+def fix_mark(child):
+    marks = Mark.objects.filter(schoolkid=child, points__in=[2, 3])
+    for mark in marks:
+        mark.points = 5
+        mark.save()
+
+
+def remove_chastisements(schoolkid):
+    chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
+    chastisements.delete()
+
+
+def get_lessons(year, letter, subject_title):
+    return Lesson.objects.filter(
+        year_of_study=year,
+        group_letter=letter,
+        subject__title=subject_title
+    )
+
+
+def create_commendation(child, subject):
+    schoolkid = get_child(child)
+
+    lessons = get_lessons(
+        schoolkid.year_of_study,
+        schoolkid.group_letter,
+        subject
+    )
+
+    for lesson in lessons:
+        Commendation.objects.create(
+            text=random.choice(COMMENDATIONS),
+            created=lesson.date,
+            schoolkid=schoolkid,
+            subject=lesson.subject,
+            teacher=lesson.teacher
+        )
